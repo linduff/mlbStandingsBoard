fcdcfc77c9759f373b757a24c28125e167df8f7d
Caleb Linduff <calinduff@gmail.com>
2026-03-29 14:33:00 -0400

    First commit with fastapi jinja2 basic layout and test data

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import json
+
+with open('testData/standings-return.json', 'r') as file:
+    data = json.load(file)
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="standings.html", context={"data": data}
+    )
